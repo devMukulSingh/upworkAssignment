9d542f64f0a66a875d89da9b049d6dde3c849e3c
devMukulSingh <mukulsingh2276@gmail.com>
2024-06-04 13:32:35 +0530

    fixed error

--- lib/localStorageProvider.ts
+++ lib/localStorageProvider.ts
@@ -1,15 +1,14 @@
 
-export function localStorageProvider() {
-  // When initializing, we restore the data from `localStorage` into a map.
-  if (typeof window !== "undefined") {
-    const map = new Map(JSON.parse(localStorage.getItem("app-cache") || "[]"));
-    // Before unloading the app, we write back all the data into `localStorage`.
-    window.addEventListener("beforeunload", () => {
-      const appCache = JSON.stringify(Array.from(map.entries()));
-      localStorage.setItem("app-cache", appCache);
-    });
-    // We still use the map for write & read for performance.
-    return map;
-  }
-  return "";
-}
+// export function localStorageProvider() {
+//   // When initializing, we restore the data from `localStorage` into a map.
+//   if (typeof window !== "undefined") {
+//     const map = new Map(JSON.parse(localStorage.getItem("app-cache") || "[]"));
+//     // Before unloading the app, we write back all the data into `localStorage`.
+//     window.addEventListener("beforeunload", () => {
+//       const appCache = JSON.stringify(Array.from(map.entries()));
+//       localStorage.setItem("app-cache", appCache);
+//     });
+//     // We still use the map for write & read for performance.
+//     return map;
+//   }
+// }
